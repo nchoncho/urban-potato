c57768d6d88bea8e47552ca1eac96d4a7fddb941
nchoncho <31830415+nchoncho@users.noreply.github.com>
2017-09-16 22:45:44 -0700

    Add files via upload

---
+++ addthreenumbers.js
@@ -0,0 +1,11 @@
+function addThreeNumbers()
+{
+    var input1 = prompt( "what is your first number?");
+    var input2 = prompt( "what is your second number?");
+    var input3 = prompt( "what is your third number?");
+
+    var result = parseInt(input1) + parseInt(input2) +parseInt(input3);
+    alert(result );
+}
+
+addThreeNumbers();
